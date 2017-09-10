64215ae6dc93ee85c1034cde9899d9b689cf40a6
Rishats <sultanov-rishat@inbox.ru>
2017-09-10 11:51:43 +0600

    Initial Commit

---
+++ Leader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Leader
+//
+//  Created by Ришат on 10.09.17.
+//  Copyright © 2017 Rishat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
