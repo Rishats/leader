4e78aa699175f1a2299bbd7ee39de1404c3fc229
Rishats <sultanov-rishat@inbox.ru>
2017-09-10 18:51:46 +0600

    add 2 button and label with info.

--- Leader/ViewController.swift
+++ Leader/ViewController.swift
@@ -10,16 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var Info: UILabel!
+    @IBAction func kandiPressed(_ sender: UIButton) {
+        Info.text = "Кандидаты"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func votesPeople(_ sender: UIButton) {
+        Info.text = "Голосующие"
     }
-
-
+    
 }
 
